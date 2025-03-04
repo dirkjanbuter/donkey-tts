7b04a1fef8a4e8be1d644b9ceeb356434a451a56
Dirk Jan Buter <djbuter@gmail.com>
2025-03-04 17:28:59 +0100

    pip

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 sudo apt-get install -y curl mpg123
 sudo apt-get install -y mecab mecab-ipadic-utf8 libmecab-dev
-sudo apt-get install -y python3.9 python3.9-venv
-python3.9 -m venv .env
-source .env/bin/activate
+sudo apt-get install -y python3.10 python3.10-venv
+python3.10 -m venv venv
+source venv/bin/activate
 pip install --upgrade pip
-pip install mecab-python3
-pip install flask pydub
-pip install networkx==2.5.0
-pip install redis
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install -r requirements.txt
 python -c "import nltk; nltk.download('punkt_tab')"
-pip install TTS
-#git clone https://github.com/coqui-ai/TTS.git
-#cd TTS
-#pip install -e .[all]
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source .env/bin/activate
+source venv/bin/activate
 uvicorn main:app --host 127.0.0.1 --port 8979
