d556effad22074b22366546dfda2ebd474c00343
Dirk Jan Buter <djbuter@gmail.com>
2025-03-14 11:32:00 +0100

    workers

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-uvicorn main:app --host 0.0.0.0 --port 8979 --timeout-keep-alive 280 --workers 4
+uvicorn main:app --host 0.0.0.0 --port 8979 --timeout-keep-alive 280 --workers 1
